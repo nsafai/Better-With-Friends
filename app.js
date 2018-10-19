01b72f59c7c5e7b2962a2ae834407507467fb8c5
Nicolai Safai <nicolaisafai@gmail.com>
2018-10-19 15:51:34 -0700

    trying bcryptjs instead of bcrypt

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
