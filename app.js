e88fa633f42d4cb83c28a9f2c920b476dd0fd403
Nicolai Safai <nicolaisafai@gmail.com>
2018-10-18 18:23:27 -0700

    fixing buggy code

--- app.js
+++ app.js
@@ -61,14 +61,17 @@ app.use(function(err, req, res, next) {
 // Database setup
 const mongoose = require('mongoose');
 const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${MONGODB_URI}`
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/event-starter', { useNewUrlParser: true });
+mongoose.connect(mongoURI, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true); // silencing a deprecated feature warning that's a bug per https://github.com/Automattic/mongoose/issues/6890
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+// Database setup
+
+// mongoose.connect(process.env.MONGODB_URI || mongoURI);
+
 // for heroku
 const port = process.env.PORT || 3000;
-app.listen(port);
 
 module.exports = app;
